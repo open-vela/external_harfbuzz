d8429311f596a760439b8a4ad7b880d8d50a6773
openvela-robot <openvela-robot@xiaomi.com>
2019-01-25 15:34:03 +0800

    Merge branch 'master' into iter

--- src/test-iter.cc
+++ src/test-iter.cc
@@ -29,19 +29,22 @@
 
 #include "hb-array.hh"
 #include "hb-set.hh"
+#include "hb-ot-layout-common.hh"
 
 
 template <typename T>
-struct array_iter_t : hb_iter_t<array_iter_t<T>, T>, hb_iter_mixin_t<array_iter_t<T>, T>
+struct array_iter_t :
+  hb_iter_t<array_iter_t<T>, T&>,
+  hb_iter_mixin_t<array_iter_t<T>, T&>
 {
   array_iter_t (hb_array_t<T> arr_) : arr (arr_) {}
 
-  typedef T __item_type__;
+  typedef T& __item_t__;
+  static constexpr bool is_random_access_iterator = true;
   T& __item_at__ (unsigned i) const { return arr[i]; }
   void __forward__ (unsigned n) { arr += n; }
   void __rewind__ (unsigned n) { arr -= n; }
   unsigned __len__ () const { return arr.length; }
-  bool __random_access__ () const { return true; }
 
   private:
   hb_array_t<T> arr;
@@ -61,6 +64,36 @@ struct some_array_t
   hb_array_t<T> arr;
 };
 
+
+template <typename Iter,
+	  hb_enable_if (hb_is_iterator (Iter))>
+static void
+test_iterator (Iter it)
+{
+  Iter default_constructed;
+
+  /* Iterate over a copy of it. */
+  for (auto c = it.iter (); c; c++)
+    *c;
+
+  it += it.len ();
+  it = it + 10;
+  it = 10 + it;
+
+  assert (*it == it[0]);
+
+  if (it.is_random_access_iterator) {}
+}
+
+template <typename Iterable,
+	 hb_enable_if (hb_is_iterable (Iterable))>
+static void
+test_iterable (const Iterable &lst = Null(Iterable))
+{
+  // Test that can take iterator from.
+  test_iterator (lst.iter ());
+}
+
 int
 main (int argc, char **argv)
 {
@@ -72,6 +105,8 @@ main (int argc, char **argv)
   array_iter_t<const int> s2 (v); /* Implicit conversion from vector. */
   array_iter_t<int> t (dst);
 
+  static_assert (hb_is_random_access_iterator (array_iter_t<int>), "");
+
   some_array_t<const int> a (src);
 
   s2 = s;
@@ -79,6 +114,23 @@ main (int argc, char **argv)
   hb_fill (t, 42);
   hb_copy (t, s);
  // hb_copy (t, a.iter ());
+
+  test_iterable (v);
+  hb_set_t st;
+  test_iterable (st);
+  hb_sorted_array_t<int> sa;
+  test_iterable (sa);
+
+  test_iterable<hb_array_t<int> > ();
+  test_iterable<hb_sorted_array_t<const int> > ();
+  test_iterable<hb_vector_t<float> > ();
+  test_iterable<hb_set_t> ();
+  test_iterable<OT::Coverage> ();
+
+  test_iterator (hb_zip (st, v));
+
+  hb_array_t<hb_vector_t<int> > pa;
+  pa->as_array ();
 
   return 0;
 }
