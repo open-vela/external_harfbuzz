56e1f3e0cf49e28c27b76b25319173a026415a3a
openvela-robot <openvela-robot@xiaomi.com>
2019-09-30 13:34:05 +0800

    Merge pull request #1807 from harfbuzz/subset-varstore
    VariationStore serializer

--- src/hb-ot-cff1-table.hh
+++ src/hb-ot-cff1-table.hh
@@ -612,7 +612,7 @@ struct CFF1StringIndex : CFF1Index
     for (unsigned int i = 0; i < strings.count; i++)
     {
       hb_codepoint_t  j = sidmap[i];
-      if (j != HB_MAP_VALUE_INVALID)
+      if (j != CFF_UNDEF_CODE)
 	bytesArray[j] = strings[i];
     }
 
@@ -630,7 +630,7 @@ struct CFF1StringIndex : CFF1Index
 
     unsigned int dataSize = 0;
     for (unsigned int i = 0; i < count; i++)
-      if (sidmap[i] != HB_MAP_VALUE_INVALID)
+      if (sidmap[i] != CFF_UNDEF_CODE)
 	dataSize += length_at (i);
 
     offSize_ = calcOffSize(dataSize);
