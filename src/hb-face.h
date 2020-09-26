2028c9323148e942d25de00c243a5f548f7a97e9
liujinye <liujinye@xiaomi.com>
2020-09-26 10:41:04 +0800

    Merge pull request #2695 from khaledhosny/docs-fix
    Minor build docs improvement

--- src/hb-face.h
+++ src/hb-face.h
@@ -46,12 +46,6 @@ hb_face_count (hb_blob_t *blob);
  * hb_face_t
  */
 
-/**
- * hb_face_t:
- *
- * Data type for holding font faces.
- *
- **/
 typedef struct hb_face_t hb_face_t;
 
 HB_EXTERN hb_face_t *
