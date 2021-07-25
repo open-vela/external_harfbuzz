8ec561840992dbb0c2aa52d99d120ba4fe7abe3e
liujinye <liujinye@xiaomi.com>
2021-07-26 00:08:34 +0800

    Merge pull request #3072 from harfbuzz/subset-docs
    [docs] Improve subset documentation

--- src/hb-subset-input.cc
+++ src/hb-subset-input.cc
@@ -60,7 +60,6 @@ hb_subset_input_create_or_fail (void)
   input->name_legacy = false;
   input->overlaps_flag = false;
   input->notdef_outline = false;
-  input->glyph_names = false;
   input->no_prune_unicode_ranges = false;
   input->retain_all_layout_features = false;
   input->passthrough_unrecognized = false;
@@ -406,8 +405,6 @@ hb_subset_input_get_flag (hb_subset_input_t *input,
       return input->passthrough_unrecognized;
     case HB_SUBSET_FLAG_NOTDEF_OUTLINE:
       return input->notdef_outline;
-    case HB_SUBSET_FLAG_GLYPH_NAMES:
-      return input->glyph_names;
     case HB_SUBSET_FLAG_NO_PRUNE_UNICODE_RANGES:
       return input->no_prune_unicode_ranges;
     default:
@@ -452,9 +449,6 @@ hb_subset_input_set_flag (hb_subset_input_t *input,
       break;
     case HB_SUBSET_FLAG_NOTDEF_OUTLINE:
       input->notdef_outline = value;
-      break;
-    case HB_SUBSET_FLAG_GLYPH_NAMES:
-      input->glyph_names = value;
       break;
     case HB_SUBSET_FLAG_NO_PRUNE_UNICODE_RANGES:
       input->no_prune_unicode_ranges = value;
