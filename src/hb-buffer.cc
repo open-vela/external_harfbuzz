4f707e8a4967c29a84f5446963d784d10468bcc2
openvela-robot <openvela-robot@xiaomi.com>
2022-01-30 17:19:46 +0800

    Merge pull request #3402 from harfbuzz/language-tags
    Make miscellaneous changes to hb-ot-tag-table.hh

--- src/hb-buffer.cc
+++ src/hb-buffer.cc
@@ -1790,7 +1790,7 @@ hb_buffer_add_codepoints (hb_buffer_t          *buffer,
  **/
 HB_EXTERN void
 hb_buffer_append (hb_buffer_t *buffer,
-		  const hb_buffer_t *source,
+		  hb_buffer_t *source,
 		  unsigned int start,
 		  unsigned int end)
 {
