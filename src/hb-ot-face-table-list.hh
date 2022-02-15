607b6ef5f195025be02412bcb92cd359de7fffa9
liujinye <liujinye@xiaomi.com>
2022-02-15 18:47:55 +0800

    Merge pull request #3411 from harfbuzz/draw
    [draw] Finish and release draw API

--- src/hb-ot-face-table-list.hh
+++ src/hb-ot-face-table-list.hh
@@ -32,11 +32,6 @@
 #define HB_OT_FACE_TABLE_LIST_HH
 #endif /* HB_OT_FACE_TABLE_LIST_HH */ /* Dummy header guards */
 
-#ifndef HB_OT_CORE_TABLE
-#define HB_OT_CORE_TABLE(Namespace, Type) HB_OT_TABLE (Namespace, Type)
-#define _HB_OT_CORE_TABLE_UNDEF
-#endif
-
 #ifndef HB_OT_ACCELERATOR
 #define HB_OT_ACCELERATOR(Namespace, Type) HB_OT_TABLE (Namespace, Type)
 #define _HB_OT_ACCELERATOR_UNDEF
@@ -51,8 +46,7 @@
 
 
 /* OpenType fundamentals. */
-HB_OT_CORE_TABLE (OT, head)
-HB_OT_CORE_TABLE (OT, maxp)
+HB_OT_TABLE (OT, head)
 #if !defined(HB_NO_FACE_COLLECT_UNICODES) || !defined(HB_NO_OT_FONT)
 HB_OT_ACCELERATOR (OT, cmap)
 #endif
@@ -80,7 +74,6 @@ HB_OT_TABLE (OT, VORG)
 #endif
 
 /* TrueType outlines. */
-HB_OT_CORE_TABLE (OT, loca) // Also used to determine number of glyphs
 HB_OT_ACCELERATOR (OT, glyf)
 
 /* CFF outlines. */
@@ -144,8 +137,4 @@ HB_OT_TABLE (OT, MATH)
 
 #ifdef _HB_OT_ACCELERATOR_UNDEF
 #undef HB_OT_ACCELERATOR
-#endif
-
-#ifdef _HB_OT_CORE_TABLE_UNDEF
-#undef HB_OT_CORE_TABLE
 #endif
