googlefonts/harfbuzz

[subset] GDEF Variation Store: step 1 closure device variation indexes, create old->new variation idx mapping and store them in subset-plan

src/hb-ot-layout.cc
@@ -318,6 +318,37 @@ hb_ot_layout_get_glyphs_in_class (hb_face_t                  *face,
   return face->table.GDEF->table->get_glyphs_in_class (klass, glyphs);
 }
 
+#ifndef HB_NO_VAR
+/**
+ * hb_ot_layout_closure_variation_indices:
+ * @face: The #hb_face_t to work on
+ * @layout_variation_indices: (inout): The #hb_set_t set of var indexes in all Device
+ * tables
+ *
+ * Fetch and remap a list of variation indices in all Device tables referenced
+ * in the specified face's GDEF table and GPOS tables that are going to be
+ * retained in the final subset
+ *
+ * Since: REPLACEME
+ **/
+void
+hb_ot_layout_closure_variation_indices (hb_face_t      *face,
+					const hb_set_t *glyphset,
+					const hb_map_t *gpos_lookups,
+					hb_set_t       *layout_variation_indices /* INOUT */,
+					hb_map_t       *layout_variation_idx_map /* INOUT */)
+{
+  if (!face->table.GDEF->table->has_data ()) return;
+  OT::hb_closure_variation_indices_context_t c (layout_variation_indices, glyphset, gpos_lookups);
+  face->table.GDEF->table->closure_variation_indices (&c);
+
+  if (hb_ot_layout_has_positioning (face))
+    face->table.GPOS->table->closure_variation_indices (&c);
+
+  face->table.GDEF->table->remap_layout_variation_indices (layout_variation_indices, layout_variation_idx_map);
+}
+#endif
+
 
 #ifndef HB_NO_LAYOUT_UNUSED
 /**

src/hb-ot-layout.h
@@ -121,6 +121,12 @@ hb_ot_layout_get_glyphs_in_class (hb_face_t                  *face,
 				  hb_ot_layout_glyph_class_t  klass,
 				  hb_set_t                   *glyphs /* OUT */);
 
+HB_EXTERN void
+hb_ot_layout_closure_variation_indices (hb_face_t *face,
+					const hb_set_t *glyphset,
+					const hb_map_t *gpos_lookups,
+					hb_set_t  *layout_variation_indices /* INOUT */,
+					hb_map_t  *layout_variation_idx_map /* INOUT */);
 
 /* Not that useful.  Provides list of attach points for a glyph that a
  * client may want to cache */

src/hb-subset-plan.cc
@@ -35,6 +35,7 @@
 #include "hb-ot-var-fvar-table.hh"
 #include "hb-ot-stat-table.hh"
 
+
 #ifndef HB_NO_SUBSET_CFF
 static inline void
 _add_cff_seac_components (const OT::cff1::accelerator_t &cff,
@@ -122,6 +123,18 @@ _gpos_closure_lookups_features (hb_face_t      *face,
 }
 #endif
 
+#ifndef HB_NO_VAR
+static inline void
+  _closure_layout_variation_indices (hb_face_t *face,
+				     const hb_set_t *glyphset,
+				     const hb_map_t *gpos_lookups,
+				     hb_set_t  *layout_variation_indices,
+				     hb_map_t  *layout_variation_idx_map)
+{
+  hb_ot_layout_closure_variation_indices (face, glyphset, gpos_lookups, layout_variation_indices, layout_variation_idx_map);
+}
+#endif
+
 static inline void
 _cmap_closure (hb_face_t           *face,
 	       const hb_set_t      *unicodes,
@@ -150,7 +163,8 @@ _populate_gids_to_retain (hb_subset_plan_t* plan,
 			  const hb_set_t *unicodes,
 			  const hb_set_t *input_glyphs_to_retain,
 			  bool close_over_gsub,
-			  bool close_over_gpos)
+			  bool close_over_gpos,
+                          bool close_over_gdef)
 {
   OT::cmap::accelerator_t cmap;
   OT::glyf::accelerator_t glyf;
@@ -209,6 +223,11 @@ _populate_gids_to_retain (hb_subset_plan_t* plan,
   }
 
   _remove_invalid_gids (plan->_glyphset, plan->source->get_num_glyphs ());
+
+#ifndef HB_NO_VAR
+  if (close_over_gdef)
+    _closure_layout_variation_indices (plan->source, plan->_glyphset, plan->gpos_lookups, plan->layout_variation_indices, plan->layout_variation_idx_map);
+#endif
 
 #ifndef HB_NO_SUBSET_CFF
   cff.fini ();
@@ -301,12 +320,15 @@ hb_subset_plan_create (hb_face_t         *face,
   plan->gpos_lookups = hb_map_create ();
   plan->gsub_features = hb_map_create ();
   plan->gpos_features = hb_map_create ();
+  plan->layout_variation_indices = hb_set_create ();
+  plan->layout_variation_idx_map = hb_map_create ();
 
   _populate_gids_to_retain (plan,
 			    input->unicodes,
 			    input->glyphs,
 			    !input->drop_tables->has (HB_OT_TAG_GSUB),
-			    !input->drop_tables->has (HB_OT_TAG_GPOS));
+			    !input->drop_tables->has (HB_OT_TAG_GPOS),
+                            !input->drop_tables->has (HB_OT_TAG_GDEF));
 
   _create_old_gid_to_new_gid_map (face,
 				  input->retain_gids,
@@ -343,6 +365,8 @@ hb_subset_plan_destroy (hb_subset_plan_t *plan)
   hb_map_destroy (plan->gpos_lookups);
   hb_map_destroy (plan->gsub_features);
   hb_map_destroy (plan->gpos_features);
+  hb_set_destroy (plan->layout_variation_indices);
+  hb_map_destroy (plan->layout_variation_idx_map);
 
 
   free (plan);

open-vela/external_harfbuzz

src/hb-ot-layout.cc
@@ -318,6 +318,37 @@ hb_ot_layout_get_glyphs_in_class (hb_face_t                  *face,
   return face->table.GDEF->table->get_glyphs_in_class (klass, glyphs);
 }
 
+#ifndef HB_NO_VAR
+/**
+ * hb_ot_layout_collect_variation_indices:
+ * @face: The #hb_face_t to work on
+ * @layout_variation_indices: (inout): The #hb_set_t set of var indexes in all Device
+ * tables
+ *
+ * Fetch and remap a list of variation indices in all Device tables referenced
+ * in the specified face's GDEF table and GPOS tables that are going to be
+ * retained in the final subset
+ *
+ * Since: REPLACEME
+ **/
+void
+hb_ot_layout_collect_variation_indices (hb_face_t      *face,
+					const hb_set_t *glyphset,
+					const hb_map_t *gpos_lookups,
+					hb_set_t       *layout_variation_indices /* OUT */,
+					hb_map_t       *layout_variation_idx_map /* OUT */)
+{
+  if (!face->table.GDEF->table->has_data ()) return;
+  OT::hb_collect_variation_indices_context_t c (layout_variation_indices, glyphset, gpos_lookups);
+  face->table.GDEF->table->collect_variation_indices (&c);
+
+  if (hb_ot_layout_has_positioning (face))
+    face->table.GPOS->table->collect_variation_indices (&c);
+
+  face->table.GDEF->table->remap_layout_variation_indices (layout_variation_indices, layout_variation_idx_map);
+}
+#endif
+
 
 #ifndef HB_NO_LAYOUT_UNUSED
 /**

src/hb-ot-layout.h
@@ -121,6 +121,12 @@ hb_ot_layout_get_glyphs_in_class (hb_face_t                  *face,
 				  hb_ot_layout_glyph_class_t  klass,
 				  hb_set_t                   *glyphs /* OUT */);
 
+HB_EXTERN void
+hb_ot_layout_collect_variation_indices (hb_face_t *face,
+					const hb_set_t *glyphset,
+					const hb_map_t *gpos_lookups,
+					hb_set_t  *layout_variation_indices /* INOUT */,
+					hb_map_t  *layout_variation_idx_map /* INOUT */);
 
 /* Not that useful.  Provides list of attach points for a glyph that a
  * client may want to cache */

src/hb-subset-plan.cc
@@ -35,6 +35,7 @@
 #include "hb-ot-var-fvar-table.hh"
 #include "hb-ot-stat-table.hh"
 
+
 #ifndef HB_NO_SUBSET_CFF
 static inline void
 _add_cff_seac_components (const OT::cff1::accelerator_t &cff,
@@ -128,6 +129,18 @@ _gpos_closure_lookups_features (hb_face_t      *face,
 }
 #endif
 
+#ifndef HB_NO_VAR
+static inline void
+  _collect_layout_variation_indices (hb_face_t *face,
+				     const hb_set_t *glyphset,
+				     const hb_map_t *gpos_lookups,
+				     hb_set_t  *layout_variation_indices,
+				     hb_map_t  *layout_variation_idx_map)
+{
+  hb_ot_layout_collect_variation_indices (face, glyphset, gpos_lookups, layout_variation_indices, layout_variation_idx_map);
+}
+#endif
+
 static inline void
 _cmap_closure (hb_face_t           *face,
 	       const hb_set_t      *unicodes,
@@ -156,7 +169,8 @@ _populate_gids_to_retain (hb_subset_plan_t* plan,
 			  const hb_set_t *unicodes,
 			  const hb_set_t *input_glyphs_to_retain,
 			  bool close_over_gsub,
-			  bool close_over_gpos)
+			  bool close_over_gpos,
+                          bool close_over_gdef)
 {
   OT::cmap::accelerator_t cmap;
   OT::glyf::accelerator_t glyf;
@@ -215,6 +229,11 @@ _populate_gids_to_retain (hb_subset_plan_t* plan,
   }
 
   _remove_invalid_gids (plan->_glyphset, plan->source->get_num_glyphs ());
+
+#ifndef HB_NO_VAR
+  if (close_over_gdef)
+    _collect_layout_variation_indices (plan->source, plan->_glyphset, plan->gpos_lookups, plan->layout_variation_indices, plan->layout_variation_idx_map);
+#endif
 
 #ifndef HB_NO_SUBSET_CFF
   cff.fini ();
@@ -308,12 +327,15 @@ hb_subset_plan_create (hb_face_t         *face,
   plan->gpos_lookups = hb_map_create ();
   plan->gsub_features = hb_map_create ();
   plan->gpos_features = hb_map_create ();
+  plan->layout_variation_indices = hb_set_create ();
+  plan->layout_variation_idx_map = hb_map_create ();
 
   _populate_gids_to_retain (plan,
 			    input->unicodes,
 			    input->glyphs,
 			    !input->drop_tables->has (HB_OT_TAG_GSUB),
-			    !input->drop_tables->has (HB_OT_TAG_GPOS));
+			    !input->drop_tables->has (HB_OT_TAG_GPOS),
+                            !input->drop_tables->has (HB_OT_TAG_GDEF));
 
   _create_old_gid_to_new_gid_map (face,
 				  input->retain_gids,
@@ -351,6 +373,8 @@ hb_subset_plan_destroy (hb_subset_plan_t *plan)
   hb_map_destroy (plan->gpos_lookups);
   hb_map_destroy (plan->gsub_features);
   hb_map_destroy (plan->gpos_features);
+  hb_set_destroy (plan->layout_variation_indices);
+  hb_map_destroy (plan->layout_variation_idx_map);
 
 
   free (plan);