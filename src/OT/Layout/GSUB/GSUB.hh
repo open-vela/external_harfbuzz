kleisauke/harfbuzz

[GSUB] Fix build on GCC < 7

src/OT/Layout/GSUB/GSUB.hh
@@ -6,9 +6,10 @@
 #include "Common.hh"
 #include "SubstLookup.hh"
 
-using OT::Layout::GSUB::SubstLookup;
-
 namespace OT {
+
+using Layout::GSUB::SubstLookup;
+
 namespace Layout {
 namespace GSUB {
 

src/hb-ot-layout-gsub-table.hh
@@ -32,9 +32,8 @@
 #include "OT/Layout/GSUB/GSUB.hh"
 
 namespace OT {
-
-using Layout::GSUB::SubstLookup;
-using Layout::GSUB::ExtensionSubst;
+namespace Layout {
+namespace GSUB {
 
 // TODO(garretrieger): Move into the new layout directory.
 /* Out-of-class implementation for methods recursing */
@@ -82,7 +81,8 @@ inline bool SubstLookup::dispatch_recurse_func<hb_ot_apply_context_t> (hb_ot_app
 }
 #endif
 
-
+} /* namespace GSUB */
+} /* namespace Layout */
 } /* namespace OT */
 
 

open-vela/external_harfbuzz

src/OT/Layout/GSUB/GSUB.hh
@@ -5,9 +5,10 @@
 #include "Common.hh"
 #include "SubstLookup.hh"
 
-using OT::Layout::GSUB_impl::SubstLookup;
-
 namespace OT {
+
+using Layout::GSUB_impl::SubstLookup;
+
 namespace Layout {
 
 /*

src/hb-ot-layout-gsub-table.hh
@@ -32,9 +32,8 @@
 #include "OT/Layout/GSUB/GSUB.hh"
 
 namespace OT {
-
-using Layout::GSUB_impl::SubstLookup;
-using Layout::GSUB_impl::ExtensionSubst;
+namespace Layout {
+namespace GSUB_impl {
 
 // TODO(garretrieger): Move into the new layout directory.
 /* Out-of-class implementation for methods recursing */
@@ -82,7 +81,8 @@ inline bool SubstLookup::dispatch_recurse_func<hb_ot_apply_context_t> (hb_ot_app
 }
 #endif
 
-
+} /* namespace GSUB_impl */
+} /* namespace Layout */
 } /* namespace OT */
 
 